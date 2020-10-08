f4adc6c2364a4400d67b7d6eacb992feb62af8b4
sylvanas <jzm362466464@126.com>
2020-10-08 23:13:13 +0800

    config router

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,14 +5,18 @@ Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
   // 按需加载
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
+  {
+    path: '/login',
+    name: 'Login',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "login" */ '@/views/login/index.vue')
+  },{
+    path: '*',
+    name: '404',
+    component: () => import(/* webpackChunkName: '404' */ '@/views/error-page/404.vue')
+  }
 ]
 
 const router = new VueRouter({
