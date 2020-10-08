74e1e3dee3808a958bc1fb59c08c0de8e3133ae1
sylvanas <jzm362466464@126.com>
2020-10-08 23:00:41 +0800

    config proxy

--- src/App.vue
+++ src/App.vue
@@ -2,24 +2,29 @@
   <div id="app">
     <h1>App</h1>
     <!-- 根级路由出口 -->
-    <router-view/> 
+    <router-view/>
   </div>
 </template>
 <script>
 import Vue from 'vue'
 // 1. 准确的类型推断
-// 2. 结合编辑器提供的类型及错误提示等辅助功能 
+// 2. 结合编辑器提供的类型及错误提示等辅助功能
 export default Vue.extend({
-	// 类型推断已启用 
-	name: 'Button', data () {
+  // 类型推断已启用
+  name: 'Button',
+  data () {
     return {
       count: 1
-		} },
+    }
+  },
   methods: {
     increment () {
-		this.count++ }
-	} 
+      this.count++
+    }
+  }
 })
 
 </script>
-<style lang="scss" scoped></style>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/utils/request.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+const request = axios.create({
+  // 配置选项
+  // baseurl
+  // timeout
+
+})
+
+// 请求拦截
+
+// 响应拦截
+
+export default request
