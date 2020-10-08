7376b4be2cddfbc26065d9a91758bd64c13cd668
sylvanas <jzm362466464@126.com>
2020-10-08 22:18:28 +0800

    import element-ui

--- src/App.vue
+++ src/App.vue
@@ -5,4 +5,21 @@
     <router-view/> 
   </div>
 </template>
+<script>
+import Vue from 'vue'
+// 1. 准确的类型推断
+// 2. 结合编辑器提供的类型及错误提示等辅助功能 
+export default Vue.extend({
+	// 类型推断已启用 
+	name: 'Button', data () {
+    return {
+      count: 1
+		} },
+  methods: {
+    increment () {
+		this.count++ }
+	} 
+})
+
+</script>
 <style lang="scss" scoped></style>
